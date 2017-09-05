2fc01bc257bb6d90e29d560b4145ea778201da4c
video2P <geffenlab@gmail.com>
2017-09-05 17:38:32 -0400

    farneback optical flow

---
+++ mouse_head_mvmt_detection.m
@@ -0,0 +1,25 @@
+%% Optical flow of mouse head videos
+% Harang Ju
+% 09/05/2017
+% Goal: find times of large head movement of a mouse
+
+%% Initialization
+directory = 'F:/videoData/';
+filename = '20170905K074_spont10mins';
+filetype = '.avi';
+
+filepath = [directory filename filetype];
+addpath(directory);
+vidReader = VideoReader(filepath);
+
+%% Optical Flow
+optFlow = opticalFlowFarneback;
+
+while hasFrame(vidReader)
+    frame = readFrame(vidReader);
+    flow = estimateFlow(optFlow, frame);
+    imshow(frame);
+    hold on
+    plot(flow,'DecimationFactor',[5 5],'ScaleFactor',2)
+    hold off
+end
