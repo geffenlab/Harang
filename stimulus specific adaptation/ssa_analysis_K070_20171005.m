701c0ba9d6c4d83ee66569427dd33a25cd4a2577
geffenlab <geffenlab@gmail.com>
2017-10-11 14:40:40 -0400

    means

--- stimulus specific adaptation/ssa_analysis_K070_20171005.m
+++ stimulus specific adaptation/ssa_analysis_K070_20171005.m
@@ -5,3 +5,12 @@ load('F:\HarangData\K070_20171005_SSA_01.mat');
 stim_dur = ceil((stimInfo.tDur+stimInfo.ICI)/1e3 * exptInfo.fr);
 raster = rasterize(spikes.raster, stim_dur, events.eventsOn);
 %%
+for i = 1 : 20
+    subplot(2,1,1)
+    imagesc(squeeze(mean(raster(1:10,:,8*(i-1)+1:8*i),2)))
+    title(['AAAAAAAA Trial ' num2str(i)])
+    subplot(2,1,2)
+    imagesc(squeeze(mean(raster(1:10,:,8*(i-1+20)+1:8*(i+20)),2)))
+    title(['BBBBBBBB Trial ' num2str(i+20)])
+    pause
+end
